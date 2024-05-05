28a90e0bfb3386c78a15f69ca60a31698f2e40cd
Sanmathi456 <sanmathi172003a@gmail.com>
2024-05-05 19:11:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// Add event listener to the "Next" button
+document.getElementById("nextButton").addEventListener("click", function() {
+    // Navigate to the next page
+    window.location.href = "familyinfo.html";
+});
+
+// Add event listener to the "Back" button
+document.getElementById("backButton").addEventListener("click", function() {
+    // Navigate to the previous page
+    window.location.href = "General.html";
+});
